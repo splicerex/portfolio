7805ac86448be17c7ab32d42051dffb78a2fc5b6
splicerex <splicerex@hotmail.com>
2018-10-26 22:18:31 -0600

    first commit

---
+++ Game/Game Start.py
@@ -0,0 +1,324 @@
+died='''
+            YYYYYYY       YYYYYYY      OOOOOOOOO      UUUUUUUU     UUUUUUUU     DDDDDDDDDDDDD       IIIIIIIIII EEEEEEEEEEEEEEEEEEEEEE DDDDDDDDDDDDD        
+            Y:::::Y       Y:::::Y    OO:::::::::OO    U::::::U     U::::::U     D::::::::::::DDD    I::::::::I E::::::::::::::::::::E D::::::::::::DDD  
+            Y:::::Y       Y:::::Y  OO:::::::::::::OO  U::::::U     U::::::U     D:::::::::::::::DD  I::::::::I E::::::::::::::::::::E D:::::::::::::::DD  
+            Y::::::Y     Y::::::Y O:::::::OOO:::::::O UU:::::U     U:::::UU     DDD:::::DDDDD:::::D II::::::II EE::::::EEEEEEEEE::::E DDD:::::DDDDD:::::D 
+            YYY:::::Y   Y:::::YYY O::::::O   O::::::O  U:::::U     U:::::U        D:::::D    D:::::D  I::::I     E:::::E       EEEEEE   D:::::D    D:::::D 
+               Y:::::Y Y:::::Y    O:::::O     O:::::O  U:::::D     D:::::U        D:::::D     D:::::D I::::I     E:::::E                D:::::D     D:::::D 
+                Y:::::Y:::::Y     O:::::O     O:::::O  U:::::D     D:::::U        D:::::D     D:::::D I::::I     E::::::EEEEEEEEEE      D:::::D     D:::::D 
+                 Y:::::::::Y      O:::::O     O:::::O  U:::::D     D:::::U        D:::::D     D:::::D I::::I     E:::::::::::::::E      D:::::D     D:::::D 
+                  Y:::::::Y       O:::::O     O:::::O  U:::::D     D:::::U        D:::::D     D:::::D I::::I     E:::::::::::::::E      D:::::D     D:::::D 
+                   Y:::::Y        O:::::O     O:::::O  U:::::D     D:::::U        D:::::D     D:::::D I::::I     E::::::EEEEEEEEEE      D:::::D     D:::::D 
+                   Y:::::Y        O:::::O     O:::::O  U:::::D     D:::::U        D:::::D     D:::::D I::::I     E:::::E                D:::::D     D:::::D 
+                   Y:::::Y        O::::::O   O::::::O  U::::::U   U::::::U        D:::::D    D:::::D  I::::I     E:::::E       EEEEEE   D:::::D    D:::::D  
+                   Y:::::Y        O:::::::OOO:::::::O  U:::::::UUU:::::::U      DDD:::::DDDDD:::::D II::::::II EE::::::EEEEEEEE:::::E DDD:::::DDDDD:::::D 
+                YYYY:::::YYYY      OO:::::::::::::OO    UU:::::::::::::UU       D:::::::::::::::DD  I::::::::I E::::::::::::::::::::E D:::::::::::::::DD 
+                Y:::::::::::Y        OO:::::::::OO        UU:::::::::UU         D::::::::::::DDD    I::::::::I E::::::::::::::::::::E D::::::::::::DDD 
+                YYYYYYYYYYYYY          OOOOOOOOO            UUUUUUUUU           DDDDDDDDDDDDD       IIIIIIIIII EEEEEEEEEEEEEEEEEEEEEE DDDDDDDDDDDDD
+                '''
+tree='''MWWWMWWWWWWWWWWWWWMWWWWMWWWWWWWWWWWWWWWWWNXKKKKKXNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWMWWWMWWWWWWWWWWWWWW
+WWMWWWWWWWWWWWWWWWWWMWWWWWWWWMWWWWWWWWN0kddddooodxxkKNWNNNXK0OkxxxkO0XWWWWWWWWWWWWWMWWWWWWWWWWWWWWWW
+MWWWWMWWWWWWWWMWWWMWWWWWWWWMWWWWWWWWWXxlloddxxxddooooxkxxdddoooddoooloONWWWWWMWWWMWWWWMWWWWWWWWMWWWM
+WWWWWWWMWWWWWWWWWWWWWWWWWWWWWWWWWWWWW0oloddxxxxxxxxdddodddddxxxxxxddolokKXNWWWWWWWWWWWWWMWWWMWWWWWWW
+MWWWWWWWWWWWWWWWWWWWWWWWWWWNX0OkkkkkOkdloddxxxxxxxxxxxxxxxxxxxxxxxxddddooddk0NWWWMWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWMWWWWWWWWWWWWWWKkoooddddoddddddxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxddollkNWWWWWWWWWWWWWMWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWKdclddddxxxxxxxxxxxxxkkxxxxxxxxxxxxxxxxxxxxxxxxxdooldXWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWXxcloodddxxxxxxxxxxxxxxxkkkxxxkkxxxxxxxxxxxxxxxxddllkXWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWXOxoooooddxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxdoolkNWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWNXK0OkdooddxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxddocxXWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWX0kdooooddddxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxdoodk0XWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWKdlodddxxdxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxdddxxxxxdxdoood0NWWWWWWWWWWWWWWWW
+WWMWWWWMWWWMWWWMMWWWWW0lloodddxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxdolodddxddddolcxNWWWWMWWWMWWWMWWW
+WWWWWWWWWMWWWWWWWWWWWWNOdlllooodddxxxxxxxxxxxdddxxxxxxxxxxdddoddddddolcllloooollldKWWWWWWWMWWWWWWWWW
+WWWWWWWWWWWMWWWWWWWWWWWWNKOkxlcloodxxxxxxxdoolooddddddddddoolllcllllcc:;;::cc::;dXWWWWWWWWWWWWWWWWWW
+WWWWWWWWWMWWWWWWWWWWWWWWWWWWW0dllodddddoolllcclllooolllllcc:::;;;;;;,;lddl::::cd0WWWWWWWWWMWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWN0xdollllllcc:;;;;;;;;;;;;;;,,;cll;,:ox0NWWNNKKXNWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWXK0xc;;;;,,;,,;llc:,,cl:,lO00xccxXWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNKkdddxk00xl:;;;;;:c;;ldoclkXWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNk:;;::c::::lx0NWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW0lcllollld0NWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNklllllllldXWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+MWWWWMWWWWWWWWWWWWWWWWWWWWWWWWWMWWWWWWWWWWWWWWNOlclllllollxXWWWWWWWWWWWWMWWWWWWWWMWWWWMWWWWWWWWWWWWM
+WWWWWWWWWWWMWWWWWWWWWWWWMWWWWMWWWWWWWWWWWWWWWNOollllllloolcdKWWWWWWWWWWWWWWWWWWMWWWWWWWWMWWWMWWWWWWW
+WWWWWWWWWMWWWWWWWWWWWWMWWWWMWWWWWWWWWWWWWWWWXklllllllllooolco0NWWWWWMWWWMWWWWWWWWWWWWWWWWWMWWWWWWWWM
+WWWWWWWMWWWMWWWWWWWWWWWWWMWWWMWWWWMWWWWWWWN0dlllllllloooloooclOXWWWWWWWWWWWWWWWWWWWWWWWWMWWWMWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWKxollllolllloooolloollkXWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNKxolooollllllloollllllllldKWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNXOdc:ccclllclllllllloxxxdllccoKWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNNKOkkOOO00000OkollcccxXNWNNKOxocoKWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+WWWMWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNXKOkOXWWWWWWWWNXKXWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+MWWWMMWWWMWWWWMWWMMWWWMMWWWMWWWMWWWWMWWWMWWWMMWNWMMWWWWWWNWMWNWMWWWWWWWWMWWWWMWWWMWWWMMWWWMWWWWMWWWM'''
+
+monster='''MMMMMMWNWMMMMMMMMMMMMMMMMMMMMMMMMMMMNkOWMMMMMMMMMMMMMMMMMMMXxcOMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMKoOWMMMMWWMMMMMMMMMMMMMMMMMMMNx:cd0WMMMMMWWWWWNNNNNXOlc:xWMMMMMMMMMMMMMMMMMMMMMMMMNXWMMK0WMMMM
+MWXXWMKlcOWWXkdKMMMMMMMMMMMMMMMMMW0dcccccdxxxdddoodoooooolc:cclxOKNWMMMMMMMMMMMMMMMNKXWMXdoKXocK0kXM
+MW0ookOl:lxxlckNXXXWMMMMMMMMMMWXOdcclccclccclcc::cllccclllcccccc:codxOXWMMMMMMMMMMMNxlok0kcldccocoXW
+MMWKd::ccc:c:cooldONMMMMMMMMNOxlcccccc::ccclc::ccc::::clc::cc:::ccclccldOXMMMMMMMMMMWOl:cc::;::,;oxo
+kdoolccllclccccd0NMMMMMMMMXklcclccccclccc:cc::cc::c::ccc::cccc:c::cc:cc::lxXWMMMMMMMMW0lccccccc:;:lk
+KOkxlccclcclclONMMMMMMMMW0lcllclc:clccccccclllccclc::clc:ccccccc:ccc::cc:::lkNMMMMMMMMWOc::clcllcdNM
+MMMXo::cccc::kWMMMMMMMMWOlcllc::clll::cc:cllllllc::cccc:clc:ccccccccccc::::cco0WMMMMMMMNx::clcc:cOMM
+MMM0lcccc:c::kWMMMMMMMWOcclcllc:coooolcccccclc:ccc::clc::clccldxkO00Oxlcc:;clcckNMMMMMMMkcclc:cc:xWM
+MMM0lcc:cc:::oXMMMMMMWOl:::cldxxdxxxkkxxoccclc::clclc:::cclddoxxdxkk0XXxcc:::cccxNMMMMMWxccc:;cc:l0M
+MMMXd:c::cccccdXMMMMW0lcl:lkKKkxlloodlcoddlclc:clcclc:clcdxxxoodddolclOXxcc::ccccxNMMMWkcccc::c::ckM
+MMMWOccc:clc:ccdKWMMKl;ccxXMKdx0kl:;;colcooccc::ccllccccdOdx0kl;'';oollkKd:c:::ccckWN0dcc:clc::::lKM
+MMMMNdccc:ccc::clx00occ:lKMWxo0Oo,   .cdlcoo::clcccl::ccloo0Oo,    ;dlco0kccc:cclccol;:cclc:c::cl0MM
+MMMMMXocccccccccccc::clccOWNxoOkd,   .ldlcdxl::clclccclccclxkd:...'oxlco0kccc:cccl:;::::cc::cc:cOWMM
+MMMMMMXdcc::cllc:c::clcccl0W0lcoxdlccldollkkcccclllc:clclkd:lddoolldollkKdcc::ccccl::cc::ccc:;c0WMMM
+MMMMMMMNxcccc:cllc;clcc:cclkX0dolcldlcllokxlccclcclccclccxKkdlccolclodO0dcc:;:cllllc::c::cc::oKMMMMM
+MMMMMMMMW0dccc::c::cc::ccc::okKKOxxxdxkxdlcclc:cc::cllccccoxxdooollodxdlccc:ccc::cclc;:c:c:lONMMMMMM
+MMMMMMMMMMW0xoc::;clccccccc:cclxk000Oxoc:cccllc:cc:clcc:cc::cccc:ccccccc:::cllcccccll;;odkOXWMMMMMMM
+MMMMMMMMMMMMMN0kl:clllccc:cllcc:ccccccc::c::::::::::::cccc:::::;;;:::::::::cc::ccllll:c0WMMMMMMMMMMM
+MMMMMMMMMMMMMMMNd:lc:c:;:::::::::::::::::::::cccc:::;;;;;;;;;::cccooddc;:c:;:cccclcccccOMMMMMMMMMMMM
+MMMMMMMMMMMMMMMXoclcc:;:ccc:;;;;;;;::::::cclc:lddddoldxkko,;xOOk:,kXXO, .:c;:ccccclcccckWMMMMMMMMMMM
+MMMMMMMMMMMMMMMXo::cl:;cc:'.      .;xOk;,x00x,:0XKOl'dXXO;  ,O0:  .xO;  .,c:;clc::cclcckMMMMMMMMMMMM
+MMMMMMMMMMMMMMMXo:ccl:;:c;.         ,OO' 'xO:  ,xc.  .ld'    ',    ..    'cc;:llcccclccOMMMMMMMMMMMM
+MMMMMMMMMMMMMMMXo:c:cc:;:c;.         ';   ..                             'cc;:llcc:clclKMMMMMMMMMMMM
+MMMMMMMMMMMMMMMNd:lccclc;:c:,.                                           ,c:;:lc:clclcoXMMMMMMMMMMMM
+MMMMMMMMMMMMMMMNd:c::cccc;;:cc,.                                        .,c:;clc:cccl:dNMMMMMMMMMMMM
+MMMMMMMMMMMMMMMWx:cc::ccccc:;:cc;'. .;c;.                       .....'...;c;:lcccllcc:xWMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMkccc:cclcc:cc:;:cc:;cxc. ..                  ..,;;;,,,;;;,,;clc::clc::kMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMOcclccccc::cclc:;::cc:,.;x:    ,,          .,,,,,,;;;,,,;;;,;clccccccl0MMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMXocc::ccc:::cccc:::::ccccc,  ,dKo.   ..   .;:;;;,'',;;;,,,;;,,:clccccoXMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMWOc::ccc:clccc:::clc::::cc:,;dO0l.  .oOc..,lxl;,;;:;,;;;;,,;;;,:c::ccxWMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWkcclc:;cc:::clcccllcc:::::::cc:,,,;ool:,;:ccccc:::;,;;;;,,;;;,;cc:l0MMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMWOlcc:cc:ccllc::cccccllc::::::::::::::::::::::::cclc;,;;;,,;;;,:c:xWMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMW0oclcclc:ccccc::cc:cclccllccc:c::::::::::ccllc:c:cc;,;;,,;;;,;:oXMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMXxccc:::c::c::clc::ccc:cc::cllc:clllc:clccccccc::l:,;;;;;;,;:oKMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMWx;:ccc::c:::cc::cc::cc::::clc:cclcccclc::::cccccc;,;,,;;:cxXMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMKl;::cccc::cl::clccccccccclcc::clccccclllcc::cl:c:;:;;:lkXWMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMXocc:::cc:clcccccc::cllc::ccc::clclc:cllc:cccccc:::::::kWMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMKo:ccc::::::cccccccclllcccllc::clc:ccllc;:::::::::c:cl:dNMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMM0:;ccccc:;::;::cdxdddooolllllcccc::cclol::c::clc::c::c;lKMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMNx:cc::cccc:::l:oXMMWWWNNXXXKKKKKKKKKKXNXoclc::lccccclc::kWMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMWOc:ccccc:cc:cllcoKMMMMMMMMMMMMMMMMMMMMMMXo:c:clccc::c:cccoKNNXXXXNMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMWXOkxdl::c::cccccccc:ccokO00XWMMMMMMMMMMMMMMMNxcclccl::cccccclcclolllllo0WMMMMMMMMMM
+MMMMMMMMMMMMMMMMKl:ccclclccclc:clclcc:ccccc:oNMMMMMMMMMMMMMN0occlccllcccllllllcllllllllckWMMMMMMMMMM
+MMMMMMMMMMMMMMMMKoccccccclcclccclddollccccco0WMMMMMMMMMMMMWxccllllcccllllcllcccccccccloONMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMNKOOOO0KX0dcc:l0MWNXK0000KNMMMMMMMMMMMMMMWOolooodxxlclccdKXK0OOkkkO0KNMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMN0xxONMMMMMMMMMMMMMMMMMMMMMMMMMMMNKXXNWMWO::ckNMMMMMMMMMMMMMMMMMMMMMMMMMM'''
+
+import time
+
+
+
+def play():
+    return start()
+
+def start():
+    print("Welcome to Grand Castle\nYour task is to make it to the castle with out dying")
+    play=input("Do you want to begin yes or no").lower()
+    if play=="yes":
+        print("Okay lets get started\n")
+        time.sleep(2)
+        return module_1()
+    elif play=="no":
+        print("Okay tell me when you want to play\n")
+        return start()
+    else:
+        print("This is not an answer try again")
+        return start()
+    
+def play_again():
+    user_1=input("Do you want to play agian")
+    if user_1=="yes\n":
+        return module_1()
+    elif user_1=="no":
+        return end()
+
+def end():
+    print("Thank you for playing hope you had fun.")
+
+
+def module_1():
+    print('You start in your house far away from the castle.\nThere is a chest.\nYou open the chest there is a sword, bow, and a doll')
+    time.sleep(5)
+    chest_take=input("What do you want the sword, bow or doll").lower()
+    if chest_take=="sword":
+        user_2=input("Are you sure").lower()
+        if user_2=="yes":
+            print("You took the sword now you are off on your adventure\n")
+            time.sleep(5)
+            return (chest_take,module_2())
+        elif user_2=="no":
+            return module_1()
+        else:
+            print("That is not a option try again\n")
+            return module_1()
+            
+    elif chest_take=="bow":
+        user_2=input("Are you sure").lower()
+        if user_2=="yes":
+            print("The bow was cursed\n")
+            time.sleep(5)
+            print(died)
+            return play_again()
+        elif user_2=="no":
+            return module_1()
+        else:
+            print("That is not a option try again\n")
+            return module_1()
+            
+    elif chest_take=="doll":
+        user_2=input("Are you sure").lower()
+        if user_2=="yes":
+            print("You took the doll now you are off on your adventure\n")
+            time.sleep(5)
+            return (chest_take,module_2())
+        elif user_2=="no":
+            return module_1()
+        else:
+            print("That is not a option try again\n")
+            return module_1()
+            
+    else:
+        print("That is not a option try again\n")
+        return module_1()
+            
+
+def module_2():
+    print('To get to the castle you have to cross a lake,\nGo throught the woods, and finally,\nGo in the crack of a tree.')
+    time.sleep(2)
+    print('You get to the edge of your proporty and there is a raft.')
+    time.sleep(4)
+    take_1=input("Do you want to take the raft").lower()
+    if take_1=="yes":
+        user_2=input("Are you sure").lower()
+        if user_2=="yes":
+            print("You took the raft\n")
+            return (module_3(),take_1)
+        elif user_2=="no":
+            return module_2()
+    elif take_1=="no":
+        user_2=input("Are you sure").lower()
+        if user_2=="yes":
+            print("You didn't take the raft\n")
+            return module_3(),take_1
+        elif user_2=="no":
+            return module_3()
+    else:
+        print("That is not a option try again")
+        return module_2()
+    
+def module_3():
+    print("You go to the trail and start heading to the lake.\nYou get about haft way to the lake when the trail splits.\n")
+    time.sleep(5)
+    user_3=input("What direction do you want to go left or right?").lower()
+    if user_3=="left":
+        user_2=input("Are you sure").lower()
+        if user_2=="yes":
+            print("You went left\n")
+            return module_4_left()
+        elif user_2=="no":
+            return module_3()
+    elif user_3=="right":
+        user_2=input("Are you sure").lower()
+        if user_2=="yes":
+            print("You went right\n")
+            return module_4_right()
+        elif user_2=="no":
+            return module_3()
+    else:
+        print("That is not a option try again")
+        return module_3()
+    
+def module_4_left():
+    print("You ran in to a tree that you could speak too.\nYou walk up to the tree and the tree speaks too you.\n")
+    print(tree)
+    user_1=input("You have 3 wishes what do you want?")
+    print("He says I grant you your wishs")
+    time_sleep(5)
+    print(died)
+    return play_again()
+
+def module_4_right():
+    print("You made it to the lake")
+    user_1=input("Do you want to us a")
+    if user_1=="sword":
+        print("You can't do anything with that")
+        return module_4_right()
+    elif user_1=="raft":
+        user_2=input("Are you sure").lower()
+        if user_2=="yes":
+            print("You went across the water and made it to the other side.\n")
+            return module_5()
+        elif user_2=="no":
+            return module_4_right()
+    elif user_1=="doll":
+        user_2=input("Are you sure").lower()
+        if user_2=="yes":
+            print("The doll came to life and killed you.\n")
+            time_sleep(5)
+            print(died)
+            time_sleep(5)
+            return start_again()
+        elif user_2=="no":
+            return module_4_right()
+        
+        
+    
+#def module_5():
+    
+    
+    
+    
+    
+
+play()
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+    
+
+                                                                                  
+                                                                                                                                          
+                                                                                                                                     
